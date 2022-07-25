17f8a347c2e255a7558784c2ca07ac61e35d1c05
Kotlyk <irashakot@gmail.com>
2022-07-25 10:11:01 +0300

    update scss files

--- src/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.reviews__slider').slick({
-        arrows: false,
-        dots: true,
-        adaptivHeight: true,
-        speed: 1000,
-       
-  });  
-});
