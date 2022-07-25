02d09d62f532d44edafb3572e3e989169dd82fe2
igr-0k <105945244+igr-0k@users.noreply.github.com>
2022-07-25 08:49:18 +0000

    Deploying to gh-pages from @ igr-0k/aci-group-project@11a0decedb421e6437afc42fd5fc897469e9d091 🚀

--- index.a6537528.js
+++
@@ -1,2 +0,0 @@
-$(".products__btn1").click((function(){$(".products__article1").fadeToggle(400)})),$(".products__btn2").click((function(){$(".products__article2").fadeToggle(400)})),$(".products__btn3").click((function(){$(".products__article3").fadeToggle(400)}));
-//# sourceMappingURL=index.a6537528.js.map
