b02912086385f78adc4c960a3cac8703f70901d1
igr-0k <105945244+igr-0k@users.noreply.github.com>
2022-07-27 15:19:18 +0000

    Deploying to gh-pages from @ igr-0k/aci-group-project@9b43252b1b24170e64efaa930a5601fb49a384c4 🚀

--- index.86202f3f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={closeMenuBtn:document.querySelector(".hom-bnt"),menu:document.querySelector(".mob-menu-wrap")};e.closeMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden")}))}();
-//# sourceMappingURL=index.86202f3f.js.map
--- index.eda12168.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={closeMenuBtn:document.querySelector(".abo-bnt"),menu:document.querySelector(".mob-menu-wrap")};e.closeMenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden")}))}();
-//# sourceMappingURL=index.eda12168.js.map
