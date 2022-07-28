decd52e164c2f7a214bb06f7c4a08eede5e2dd82
igr-0k <105945244+igr-0k@users.noreply.github.com>
2022-07-28 09:50:52 +0000

    Deploying to gh-pages from @ igr-0k/aci-group-project@331c8dc4f3fd9a4a82cc27773a8c004cbc5e72ea 🚀

--- index.54e2e618.js
+++
@@ -1,2 +0,0 @@
-$("a").bind("click",(function(t){var a=$(this);$("html, body").stop().animate({scrollTop:$(a.attr("href")).offset().top-70+"px"},750),t.preventDefault(),$(".active").removeClass("active"),$(this).addClass("active")}));
-//# sourceMappingURL=index.54e2e618.js.map
