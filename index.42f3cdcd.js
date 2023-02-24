b3b3e150ccb9f2815edf073030cdbad76bd53626
ur-Hanna <104525960+ur-Hanna@users.noreply.github.com>
2023-02-24 10:04:17 +0000

    Deploying to gh-pages from @ igr-0k/aci-group-project@d4082c72f9b1c66ab8e37920ccf77ed8f61a5bd6 🚀

---
+++ index.42f3cdcd.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".products__list button").forEach((function(t){return t.addEventListener("click",(function(e){e.currentTarget===t&&t.classList.toggle("is-open")}))}));
+//# sourceMappingURL=index.42f3cdcd.js.map
---
+++ index.bdfa708f.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".products__list button").forEach((t=>t.addEventListener("click",(e=>{e.currentTarget===t&&t.classList.toggle("is-open")}))));
+//# sourceMappingURL=index.bdfa708f.js.map
